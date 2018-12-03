a4206fe0505d9bc6cf6c01477d6d51b3ee7a5b7c
jacky.leung <jacky.leung@oocl.com>
2018-12-03 17:19:40 +0800

    [LEUNGJA] feat: add line component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import NumberTxt from './NumberTxt';
+import NumberTxt from './Components/NumberTxt';
+import Line from './Components/Line';
 
 class App extends Component {
-  state = {number : this.props.number};
+  state = {
+    number : this.props.number, 
+    numberArray : new Array(this.props.number).fill(0)
+  };
 
   alertButtonClick = () => {
     // alert("now you know how to handle onclick event");
     let newNumber = this.props.onClickAddTwo(this.state.number);
-    this.setState({number : newNumber});
-  };
+    this.setState({
+      number : newNumber, numberArray : new Array(newNumber).fill(0)
+    });
+    console.log(this.state.numberArray);
+    // this.renderLine();
+  }
+
+  renderLine = () => {
+    this.state.numberArray.map(() => 
+      <Line />
+    )
+  }
 
   render() {
     return (
       <div className="App">
         <button onClick={this.alertButtonClick}>Hello World!</button>
         <span><NumberTxt /> {this.state.number}</span>
+        {/* <div>{this.renderLine}</div> */}
+        <div>
+            {this.state.numberArray.map(() => <Line />)}
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 // ReactDOM.render(<App number={"100"} onClickWho={() => setState({number : state.number * 2})} />, document.getElementById('root'));
-ReactDOM.render(<App number={100} onClickAddTwo={(number) => number + 2} />, document.getElementById('root'));
+ReactDOM.render(<App number={1} onClickAddTwo={(number) => number + 2} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
